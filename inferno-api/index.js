bddb96da92fb7cf67badd1a1195af15d94ad8a93
Elie <jacques.le.lezard@gmail.com>
2018-10-01 14:39:52 +0200

    Inferno app, initial commit

---
+++ inferno-api/index.js
@@ -0,0 +1,115 @@
+//config--------------------------------
+const express = require('express')
+const app = express()
+const port = 3001
+var algoliasearch = require('algoliasearch');
+var fs = require('fs');
+var client = algoliasearch('N2K83PZDUD', 'f0ef21e4688fd0d4b417e283981c8c12');
+var index = client.initIndex('inferno');
+var appJSON = require('./data.json');
+
+
+//routing--------------------------------
+//check if everything is fine
+app.get('/', function (req, res) {
+    res.send('Hello, welcome to the test api, use POST/DELETE to /api/1/apps to add/remove an app')
+})
+
+//Post a new app
+app.post('/api/1/apps', function (req, res) {
+    if (req.query.newApp) {
+        var newApp = JSON.parse(req.query.newApp);
+        if (newApp.name && newApp.image && newApp.link && newApp.category && newApp.rank) {
+            readData2(addData, newApp);
+            res.send('Adding the app ' + appId);
+        } else {
+            res.send('You miss one of the following parameters: name, image, link, category, rank');
+        }
+    } else {
+        res.send('Your app should have the following structure: newApp: {name: "name of my app", image: "url of the image", link: "link to the app", category: "main category of your app", rank: "rank of your app}');
+    }
+
+
+})
+
+//Search for a specific term
+app.get('/api/1/search/:term', function (req, res) {
+    index.search(req.params.term, function (err, content) {
+        res.send(content.hits);
+    });
+})
+
+//Delete an existing app by id
+app.delete('/api/1/apps/:appId', function (req, res) {
+    if (req.query.appId) {
+        var appId = req.query.appId;
+        readData2(removeData(removeData, appId))
+    } else {
+        res.send("You miss the appId parameter")
+    }
+})
+
+//launch server
+app.listen(port, () => console.log(`Inferno api listening on port ${port}!`))
+
+
+//functions--------------------------------
+function indexData(jsonFile) {
+    index.addObjects(jsonFile, function (err, content) {
+        if (err) {
+            console.error(err);
+        }
+        console.log("The apps have been indexed");
+    });
+}
+
+//function readData() {
+//    fs.readFile('./data.json', 'utf8', function (err, data) {
+//        if (err) {
+//            return console.log(err);
+//        }
+//        data = JSON.parse(data);
+//        console.log(data[0]);
+//        return data;
+//    });
+//}
+
+function readData2(callback, newData) {
+    fs.readFile('./data.json', 'utf8', function (err, data) {
+        if (err) {
+            return console.log(err);
+        }
+        data = JSON.parse(data);
+        callback(data, newData);
+    });
+
+}
+
+function addData(data, newData) {
+    //add data to the json
+    data.push(newData);
+    fs.writeFile('./data.json', JSON.stringify(data), function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("The app has been added to the local database");
+    });
+    //update the index
+    index.addObjects([newData], function (err, content) {
+        console.log("The app has been indexed");
+        console.log(content);
+        return (content.objectIDs[0]);
+    });
+}
+
+
+function removeData(id) {
+    //removed data from the json
+    //TODO
+    //update the index
+    index.deleteObjects([id], function (err, content) {
+        if (err) throw err;
+        console.log(content);
+        console.log("The app has been removed from index");
+    });
+}
---
+++ inferno-app/src/App.js
@@ -0,0 +1,186 @@
+import {
+    version,
+    Component
+} from 'inferno';
+import './registerServiceWorker';
+import logo from './logo';
+import './App.css';
+var algoliasearch = require('algoliasearch');
+var client = algoliasearch('N2K83PZDUD', '35b4d2caced22b7b1d894cc2701eae18');
+var index = client.initIndex('inferno');
+
+
+class App extends Component {
+    constructor() {
+        super();
+    };
+    render() {
+        return ( < div className = "App" >
+            <
+            header className = "App-header" > <
+            p >
+            Looking
+            for a new app ? <
+            /p><
+            h1 >
+            You will find it here <
+            /h1> <
+            video src = "https://staging.coverr.co/s3/mp4/Indian%20Tea.mp4"
+            loop autoplay > < /video>   < /
+            header > <
+            NameForm > < /NameForm>   < /
+            div >
+        );
+    };
+};
+
+
+
+
+class NameForm extends App {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: '',
+            category: '',
+            displayedItems: [],
+            categoriesList: ["All", "Books", "Business", "Games", "Navigation", "Newsstand", "Social Networking", "Medical", "News", "Catalogs", "Education", "Sports", "Music", "Photo & Video", "Travel", "Finance", "Productivity", "Entertainment"],
+            sortOrder: 'asc',
+        };
+        //this.displayedItems = [];
+        console.log(this.state.categoriesList);
+        this.handleChange = this.handleChange.bind(this);
+        index.setSettings({
+            'searchableAttributes': [
+            'name', 'category', 'rank'
+          ],
+            replicas: [
+                "ascendentRanking",
+                "decendentRanking"
+            ],
+            'attributesForFaceting': ['category'],
+            'customRanking': [this.sortOrder + '(rank)']
+        }, {
+            forwardToReplicas: true
+        }, function (err, content) {
+            console.log(content);
+        });
+    }
+    handleChange = (event) => {
+        this.setState({
+            value: event.target.value
+        });
+        this.search();
+    }
+    handleChangeSelect = (event) => {
+        this.setState({
+            category: event.target.value
+        });
+        this.search();
+    }
+    reverseSorting = () => {
+        if (this.sortOrder == "desc") {
+            this.sortOrder = "asc";
+        } else {
+            this.sortOrder = "desc"
+        }
+        let sorting = this.sortOrder + '(rank)'
+        index.setSettings({
+            'customRanking': [sorting]
+        }, {
+            forwardToReplicas: true
+        })
+    }
+
+
+    search = () => {
+        if (this.state.category != "" && this.state.category != "All") {
+            console.log("searching for " + this.state.value + " in " + this.state.category + " category");
+            index.search(this.state.value, {
+                filters: 'category:' + this.state.category
+            }, (err, content) => {
+                if (content) {
+                    console.log(content.hits);
+                    this.setState({
+                        displayedItems: content.hits
+                    });
+                } else {
+                    console.log("no item found")
+                }
+
+            });
+        } else {
+            console.log("searching for " + this.state.value + " in all categories");
+            index.search(this.state.value, (err, content) => {
+                if (content) {
+                    console.log(content.hits);
+                    this.setState({
+                        displayedItems: content.hits
+                    });
+                } else {
+                    console.log("no item found")
+                }
+            });
+        }
+
+    }
+
+    render() {
+            return ( < section class = "searchArea" > < form onSubmit = {
+                    this.handleSubmit
+                } >
+                <
+                label >
+                Search
+                for: < /label> <
+                input type = "text"
+                value = {
+                    this.state.value
+                }
+                placeholder = "books?"
+                onInput = {
+                    this.handleChange
+                }
+                />   <
+                label
+                for = "title" > Filter by: < /label>
+
+
+                <
+                select id = "category"
+                name = "category"
+                onInput = {
+                    this.handleChangeSelect
+                } > {
+                    this.state.categoriesList.map((cat) => < option value = {
+                            cat
+                        } > {
+                            cat
+                        } < /option>)}</select > < a class = "sort-button"
+                        onClick = {
+                            this.reverseSorting
+                        } > Reverse order < /a> <
+                        div class = "store" > {
+                            this.state.displayedItems.map(function (item) {
+                                item.bgimage = "background-image:url(" + "https://picsum.photos/400/400/?image=" + item.rank + ")"
+                                return <div class = "item"
+                                onClick = {
+                                    function () {
+                                        window.open(item.link)
+                                    }
+                                }
+                                style = {
+                                    item.bgimage
+                                } > < h2 > {
+                                    item.name + " " + item.rank
+                                } < /h2> < /div >
+                            })
+                        } < /div > < /form > < /
+                        section >
+                    );
+                }
+            };
+
+            var oldData = ['red', 'green', 'blue'];
+
+            export default App;
